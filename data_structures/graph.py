c2ef4cf12483fbe76c2f4569cab767faa2afa9c7
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2021-11-28 23:01:26 +0530

    graph ds: add edge function

--- data_structures/graph.py
+++ data_structures/graph.py
@@ -22,8 +22,7 @@ class Vertex:
     def print_edges(self):
         str_edges = ''
         for edge in self.edge_list:
-            str_edges += f'{edge.destination_vertex_id} '
-
+            str_edges += f'{edge.destination_vertex_id}({edge.weight}) '
         return str_edges
 
 
@@ -36,9 +35,48 @@ class Graph:
         for vertex in self.vertices:
             print(f'{vertex.name} ({vertex.id}) -> [{vertex.print_edges()}]')
 
+    def get_vertex(self, v_id):
+        for vertex in self.vertices:
+            if vertex.id == v_id:
+                return vertex
+        return None
+
+    def check_vertex_exists(self, v_id):
+        for vertex in self.vertices:
+            if vertex.id == v_id:
+                return True
+        return False
+
+    def check_edge_exists(self, start_v_id, end_v_id):
+        start_v = self.get_vertex(start_v_id)
+        for edge in start_v.edge_list:
+            if edge.destination_vertex_id == end_v_id:
+                return True
+        return False
+
     def add_vertex(self, new_vertex):
         self.vertices.append(new_vertex)
-        print(f'vertex added succesfully')
+        print(f'vertex({new_vertex.id}, {new_vertex.name}) added succesfully')
+
+    def add_edge(self, start_v_id, end_v_id, weight):
+        is_start_v_exists = self.check_vertex_exists(start_v_id)
+        is_end_v_exists = self.check_vertex_exists(end_v_id)
+
+        if not is_start_v_exists or not is_end_v_exists:
+            return print(f'Invalid vertex id!')
+
+        if self.check_edge_exists(start_v_id, end_v_id):
+            return print(f'edge already exists between {self.get_vertex(start_v_id).name} & {self.get_vertex(end_v_id).name}')
+
+        for vertex in self.vertices:
+            if vertex.id == start_v_id:
+                edge = Edge(end_v_id, weight)
+                vertex.edge_list.append(edge)
+            elif vertex.id == end_v_id:
+                edge = Edge(start_v_id, weight)
+                vertex.edge_list.append(edge)
+
+        print(f'Edge added between {self.get_vertex(start_v_id).name} & {self.get_vertex(end_v_id).name}')
 
 
 def main():
@@ -46,8 +84,14 @@ def main():
 
     v1 = Vertex('TN')
     v2 = Vertex('KA')
+    v3 = Vertex('MH')
     g.add_vertex(v1)
     g.add_vertex(v2)
+    g.add_vertex(v3)
+    g.display()
+    g.add_edge(1,2,70)
+    g.add_edge(1,3,50)
+    g.add_edge(1,3,50)
     g.display()
 
 if __name__ == "__main__":
